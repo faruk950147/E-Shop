3d983862b9481cee928bffd6a178e5f7a2a5b58c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-23 09:27:07 +0600

    It's my live portfolio

--- store/models.py
+++ store/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from decimal import Decimal
-from django.utils.text import slugify
-from django.utils.html import format_html
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
