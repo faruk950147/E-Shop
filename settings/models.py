bc5bdf6c9c8df7444e7bf8c1454cc4725f87c6fa
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-24 13:06:45 +0600

    It's my live portfolio

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from decimal import Decimal
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+    EmailValidator,
+)
 
-# Create your models here.
+User = get_user_model()
+
+from common.common import (
+    BaseMixin, CommonMixin 
+)
+
+from mixins.mixins import (
+    LoginRequiredMixin, ImageTagMixin, StripMixin, ColorTagMixin 
+)
+from validation.validators import (
+    validate_image_size, validate_file_extension
+)
+
