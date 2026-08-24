10104d6d14ba751ec57a28be2978274a114e8eed
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-24 15:24:34 +0600

    It's my live portfolio

--- setting/admin.py
+++ setting/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from setting.models import Logo, Footer, Link
 
 
+@admin.register(Logo)
+class LogoAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "image_tag", "status", "created_at", "updated_at")
+    list_display_links = ("id", "title")
+    list_filter = ("status", "created_at", "updated_at")
+    search_fields = ("title",)
+    readonly_fields = ("image_tag", "created_at", "updated_at")
+
+    fieldsets = (
+        (None,
+            {"fields": ("title", "image", "image_tag", "status", "created_at", "updated_at"),},
+        ),
+    )
+
+
+
 @admin.register(Footer)
 class FooterAdmin(admin.ModelAdmin):
     list_display = ("id", "email", "phone", "copyright_text", "status", "created_at", "updated_at")
