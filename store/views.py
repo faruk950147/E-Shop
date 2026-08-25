5ed00a2ea496566e603388d5af6c2e9c8510ff57
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-25 17:33:25 +0600

    It's my live portfolio

--- store/views.py
+++ store/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views import View
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext as _, get_language
 
 
 class HomeView(View):
 
     def get(self, request):
+        current_lang = get_language()
         message = _("Hello, world!")
 
-        return render(request, "store/home.html", {"message": message})
+        return render(request, "store/home.html", {"current_lang": current_lang, "message": message})
