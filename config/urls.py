1962f7adf2636282e340aa2e0d33c0de9bf524a9
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-25 06:47:56 +0600

    It's my live portfolio

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,25 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
-    path('', include('store.urls')),
-    path('account/', include('account.urls')),
-    
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('admin/', admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("admin/", admin.site.urls),
 ]
+
+urlpatterns += i18n_patterns(
+    path("", include("store.urls")),
+    path("account/", include("account.urls")),
+)
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
