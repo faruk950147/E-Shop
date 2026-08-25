5997fd0630d3c27a3bf00ed1969f26b1114cecd7
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-25 23:32:03 +0600

    It's my live portfolio

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.utils.translation import gettext as _, get_language
+from .models import Category
 
 
 class HomeView(View):
