3143d20d46f45869248cb0ff6343b7998cb4353a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-25 10:56:20 +0600

    It's my live portfolio

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 # Custom context processors 
+                'setting.context_processors.site_settings',
                 'store.context_processors.store_context',  
                 # 'cart.context_processors.cart_context',
             ],
